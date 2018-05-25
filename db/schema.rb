04d22335a85009074dd5f518763ca7429e80e848
Ally Netzel <netzelally@gmail.com>
2018-05-25 17:19:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 5) do
     t.string "name"
     t.integer "show_id"
     t.integer "actor_id"
+    t.string "catchphrase"
+    t.string "build_show"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -32,6 +34,9 @@ ActiveRecord::Schema.define(version: 5) do
     t.string "name"
     t.string "genre"
     t.integer "network_id"
+    t.string "day"
+    t.string "season"
+    t.string "build_network"
   end
 
 end
